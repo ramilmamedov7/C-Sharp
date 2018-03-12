9997a6daf3955c4962026feafe85fbbd8c96cb2b
Rommel7 <ramil.mamedov7991@gmail.com>
2018-03-12 21:35:58 +0400

    Filer Remover Console

--- C# - 13 (WorkingWithFiles - Console)/WorkingWithFiles/Program.cs
+++ C# - 13 (WorkingWithFiles - Console)/WorkingWithFiles/Program.cs
@@ -9,27 +9,31 @@ namespace WoorkingWithFiles
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.Title = "07.10.2017";
+            Console.Title = "Working With Files";
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Clear();
 
-            #region SoundPlayer
-            var music = new soundplayer();
-            string path = @"c:\users\ramil\desktop\workingwithfiles\slide.wav";
-            music.soundlocation = path;
-            music.play();
-            console.read();
+            //Opens default music player app on your system.
+            System.Diagnostics.Process.Start(@"D:\Drive\Paradise.wav");
+
+            #region SoundPlayer [Plays music in console.]
+            var Music = new SoundPlayer();
+            //Give source to your music file.
+            string Path = @"D:\Drive\Slide.wav";
+            Music.SoundLocation = Path;
+            Music.Play();
+            Console.Read();
             #endregion
-            //System.Diagnostics.Process.Start(@"C:\Users\Ramil\Desktop\Lab\Paradise.wav");
 
             #region New Document
-            var document = new StreamWriter(@"C:\Users\Ramil\Desktop\Lab\text.txt");
-            string words;
-            Console.WriteLine("Ürək sözlərinizi daxil edin: ");
-            words = Console.ReadLine();
-            document.WriteLine(words);
-            document.Close();
+            //Give source to your text file.
+            var Document = new StreamWriter(@"D:\Drive\text.txt");
+            string Words;
+            Console.WriteLine("Please write your message: ");
+            Words = Console.ReadLine();
+            Document.WriteLine(Words);
+            Document.Close();
             #endregion
         }
     }
--- C# - 13 (WorkingWithFiles - Console)/WorkingWithFiles/Properties/AssemblyInfo.cs
+++ C# - 13 (WorkingWithFiles - Console)/WorkingWithFiles/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("WorkingWithFiles")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WorkingWithFiles")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("62845fa5-d4da-4270-8034-c194c314b198")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c16068e9-bd6a-4327-bdd3-7e2428613c9a")]
 
-// Сведения о версии сборки состоят из следующих четырех значений:
+// Version information for an assembly consists of the following four values:
 //
-//      Основной номер версии
-//      Дополнительный номер версии
-//   Номер сборки
-//      Редакция
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
 //
-// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
-// используя "*", как показано ниже:
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
